148541393220fb09117eaea40f43bc8f8c848947
divya <divyansh.srivastavadps@gmail.com>
2019-04-11 14:09:31 +0530

    User, Editor, Staticfiles added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'glfmfp+d7uv0c09j68((100!i$zlqb7&01t^0%bp(ugionvrqp'
+SECRET_KEY = ' '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
